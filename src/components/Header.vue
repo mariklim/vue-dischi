299e9aca472ebf43f89dae4d4f532650b7ff3a1e
mariklim <maria.klimenko889@gmail.com>
2021-10-01 16:50:46 +0200

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<section>
+<h1>Header</h1>
+</section>
+</template>
+
+<script>
+export default {
+    name: "Header"
+
+}
+</script>
+
+<style lang="scss">
+h1{
+    color: brown;
+}
+</style>
